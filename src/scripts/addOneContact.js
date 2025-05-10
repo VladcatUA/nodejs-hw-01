d2caf1e5e5c6092d589dec6328b802b7d0b7a0c8
Vladyslav <vladislav2002vladislav2002@gmail.com>
2025-05-10 13:53:11 +0300

    noda-hw1-commit3

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,25 +1,37 @@
 import { createFakeContact } from '../utils/createFakeContact.js';
+import { writeContacts } from '../utils/writeContacts.js';
 import { readContacts } from '../utils/readContacts.js';
 
 export const addOneContact = async () => {
   try {
     const oldContacts = await readContacts();
-    const newContact = createFakeContact();
-
-    oldContacts.push(newContact);
+    const newContacts = [...oldContacts, createFakeContact()];
+    await writeContacts(newContacts);
+    console.log('Contact added successfully');
   } catch (error) {
-    console.log(error.message);
+    console.error('Error adding contact:', error);
   }
 };
 
 addOneContact();
 
 
-//mport { generateContacts } from './generateContacts.js';
+
+
+//import { createFakeContact } from '../utils/createFakeContact.js';
+//import { readContacts } from '../utils/readContacts.js';
 
 //export const addOneContact = async () => {
-//  await generateContacts(1);
+ // try {
+ //   const oldContacts = await readContacts();
+//    const newContact = createFakeContact();
+
+//    oldContacts.push(newContact);
+ // } catch (error) {
+ //   console.log(error.message);
+ // }
 //};
 
 //addOneContact();
+
 
